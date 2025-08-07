2027ea49024ada1e1009a3154da3e7b92feff00c
HARSORA YASHVI <yashviharsora1710@gmail.com>
2025-08-07 11:34:05 +0530

    Add files via upload

---
+++ t3_q5.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJT
+{
+    
+
+        class Product
+
+        {
+
+        int pcode;
+        String pname, mname;
+
+        public Product(int pcd, String pnm, String mnm)
+
+            {
+
+                mname = mnm;
+
+                pcode = pcd;
+
+                pname = pnm;
+
+        }
+
+
+
+            public void Display()
+
+            {
+
+                Console.WriteLine("\nManufacturer Name:= " + mname);
+
+                Console.WriteLine("\nProduct Code:= " + pcode);
+
+                Console.WriteLine("\nProduct Name:= " + pname);
+
+        }
+
+    }
+
+
+            
+
+
+
+ 
+
+ 
+
+    public class TestProduct
+
+        {
+
+            public static void Main(string[] args)
+
+            {
+
+                int n = args.Length;
+
+
+
+                if (n < 3)
+
+                {
+
+                    Console.WriteLine("Syntax Error\n");
+
+                    Console.WriteLine("Must Have THREE Arguments\n");
+
+                    Console.WriteLine("Please, Write as [csc TestProduct ProductCode ProductName Manufacturer] \n");
+
+                }
+
+                else
+
+                {
+
+
+                int pcd = Convert.ToInt32(args[0]);
+
+                String pnm = args[1];
+
+                String mnm = args[2];
+
+                Product p = new Product(pcd, pnm, mnm);
+
+                p.Display();
+
+                    Console.Read();
+
+
+
+
+                }
+
+            }
+
+        }
+
+    
+}
