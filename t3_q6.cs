b863d3b0e96f60bc97e41feeebf39a4331a95a34
HARSORA YASHVI <yashviharsora1710@gmail.com>
2025-08-07 11:28:36 +0530

    Add files via upload

---
+++ t3_q6.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PJT
+{
+
+
+namespace LineApplication
+
+    {
+
+        class Line
+
+        {
+
+            private double length;   // Length of a line
+
+            public Line()
+
+            {
+
+                length = 12;
+                Console.WriteLine("length is " + length);
+
+                //………………………………Missing statement-2……………………………….// 
+
+            }
+
+
+
+            public void setLength(double len)
+
+            {
+                length = len;
+
+                //………………………………Missing statement-3……………………………….//      
+
+
+
+            }
+
+            public double getLength()
+
+            {
+                return length;
+
+                //………………………………Missing statement-4……………………………….//      
+
+            }
+
+        }
+
+
+
+        class TestLine
+
+        {
+
+            static void Main(string[] args)
+
+            {
+
+                Line line = new Line();
+
+                // set line length
+                line.setLength(20);
+
+                Console.WriteLine("Length of line : {0}", line.getLength());
+
+                // set line length
+                line.setLength(30);
+
+                Console.WriteLine("Length of line : {0}", line.getLength());
+
+                Console.ReadKey();
+
+            }
+
+        }
+
+    }
+
+}
